791bd6eb23ce313bad67f27e7c65373a1e41b44e
keerthana1404 <119326359+keerthana1404@users.noreply.github.com>
2024-10-01 11:31:03 +0530

    Update userRouter.js

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -7,7 +7,7 @@ const router= express.Router();
 router.post("/register",register);
 router.post("/login",login);
 router.get("/logout",isAuthenticated,logout);
-router.get("/getUser",isAuthenticated,getUser);
+router.get("/getuser",isAuthenticated,getUser);
 router.put("/update/profile",isAuthenticated,updateProfile);
 router.put("/update/password",isAuthenticated,updatePassword);
 
